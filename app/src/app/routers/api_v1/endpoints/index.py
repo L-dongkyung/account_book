af107cd11039b4af74fbd8ec1e59499a74c7d31e
L-dongkyung <ckldk91m@naver.com>
2022-12-21 16:38:42 +0900

    Feature : get user

--- app/src/app/routers/api_v1/endpoints/index.py
+++ app/src/app/routers/api_v1/endpoints/index.py
@@ -5,6 +5,9 @@ from sqlalchemy.orm import Session
 from starlette.responses import Response
 
 from db import db
+from routers.auth import get_current_user
+from models.user import User
+from schemas import user_schema
 
 router = APIRouter()
 
@@ -18,4 +21,6 @@ async def index(session: Session = Depends(db.session)):
     return Response(f"fast api normal (UTC: {current_time}, DB_version: {session})", status_code=200)
 
 
-
+@router.get("/me/", response_model=user_schema.User)
+async def get_me(current_user: User = Depends(get_current_user)):
+    return current_user
--- app/src/app/schemas/user_schema.py
+++ app/src/app/schemas/user_schema.py
@@ -5,3 +5,8 @@ class CreateUser(BaseModel):
     email: str
     password: str
 
+class User(BaseModel):
+    email: str
+
+    class Config:
+        orm_mode = True
